b2c6c8bf07e6497456fe84fe328dd6292b177673
Documenter.jl <documenter@juliadocs.github.io>
2023-07-28 08:06:09 +0000

    build based on a11c8f6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SFilters","category":"page"},{"location":"#SFilters","page":"Home","title":"SFilters","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SFilters.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SFilters]","category":"page"}]
+}
